95a392b9eedbc9a601f1917699037d0124d70268
Carol <carolleigh.phiri@gmail.com>
2025-08-03 22:44:15 +0200

    Final change for Compulsory Task 2

---
+++ HelloWorld.y
@@ -0,0 +1,1 @@
+"# This is a new change only on issue1 branch"
--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,5 @@
-'print("Hello World")'
-'print("Git is awesome!")'
+# This program greets the user 
+name = input("Enter your name: ")
+print(f"Hello, {user}!")
+
+"# This is a final change for Compulsory Task 2"
