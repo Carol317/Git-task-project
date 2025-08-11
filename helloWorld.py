1b0dbd990793262439f4f167c4e67273a0db817d
Carol <carolleigh.phiri@gmail.com>
2025-08-11 20:45:02 +0200

    refactor helloworld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,4 @@
 # This program greets the user 
 name = input("Enter your name: ")
-print(f"Hello, {user}!")
-
-"# This is a final change for Compulsory Task 2"
-""
-"name = input(PROMPT_MESSAGE)" 
-"print(GREETING_TEMPLATE.format(name))" 
+print(f"Hello, {name}!")
+ 
