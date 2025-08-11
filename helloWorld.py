4a178dab215026288d7edf951bfebd1469218a91
Carol <carolleigh.phiri@gmail.com>
2025-08-11 20:31:54 +0200

    Accept and use input

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,8 @@
-print("Git is awesome")
+# This program greets the user 
+name = input("Enter your name: ")
+print(f"Hello, {user}!")
+
+"# This is a final change for Compulsory Task 2"
+""
+"name = input(PROMPT_MESSAGE)" 
+"print(GREETING_TEMPLATE.format(name))" 
